58c217f5a92eb9eb81627ba50dd07c546c36b1c7
waterricecake <fruturum@nate.com>
2023-04-16 16:46:10 +0900

    [BOJ] 13305/Silver3/15M

--- dali/week5/Silver.java
+++ dali/week5/Silver.java
@@ -1,42 +1,50 @@
-package week1;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
-public class Silver {
-    static int N;
-    static int M;
-    static int[] arr;
+public class Main {
+    private static int N;
+    private static long[] dis;
+    private static long[] price;
+    private static long total;
     public static void main(String[] args) throws IOException {
         game(new InputStreamReader(System.in));
     }
 
     public static void game(InputStreamReader isr) throws IOException {
         BufferedReader br = new BufferedReader(isr);
+
+        N = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        dis = new long[N-1];
+        for (int i = 0; i < N-1; i++) {
+            dis[i] = Long.parseLong(st.nextToken());
+        }
+
         st = new StringTokenizer(br.readLine());
-        arr = new int[N];
+        price = new long[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            price[i] = Long.parseLong(st.nextToken());
         }
-        int result= 0;
-        int total = 0;
-        int end = 0;
-        for (int start = 0; start < N; start++) {
-            while(total<M && end<N){
-                total+=arr[end];
-                end++;
-            }
-            if(total == M){
-                result++;
-            }
-            total -= arr[start];
+
+        total = 0;
+        total += price[0] * dis[0];
+        long now = price[0];
+        if(N==2){
+            System.out.println(total);
+            return;
         }
-        System.out.println(result);
+        for (int i = 1; i < N-1; i++) {
+            if(price[i]>now){
+                total+= now * dis[i];
+                continue;
+            }
+            now = price[i];
+            total+= now * dis[i];
+        }
+        System.out.println(total);
     }
 }
