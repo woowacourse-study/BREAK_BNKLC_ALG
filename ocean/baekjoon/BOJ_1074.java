a86dc930381c572812c302c87792cfb62e872174
donghae <donghae1999@gmail.com>
2023-04-23 01:36:02 +0900

    [BOJ] Z / Silver1 / 30m

--- ocean/baekjoon/BOJ_1074.java
+++ ocean/baekjoon/BOJ_1074.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ1074 {
+public class BOJ_1074 {
     private static int answer = 0;
 
     public static void main(String[] args) throws IOException {
